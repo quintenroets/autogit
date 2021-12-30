d1bfabe9fc422edd5c5c667e907a0de7ff09ca8a
Quinten <quinten.roets@gmail.com>
2021-12-30 15:59:48 +0100

    set konsole title

--- autogit/main.py
+++ autogit/main.py
@@ -1,5 +1,6 @@
 import argparse
 
+from libs.cli import Cli
 from libs.errorhandler import ErrorHandler
 
 from .git import GitManager
@@ -8,6 +9,8 @@ def main():
     parser = argparse.ArgumentParser(description='Automate common git workflows')
     parser.add_argument('action', nargs="?", help='The action to do', default="refresh")
     parser.add_argument('names', nargs='*', help='repository names')
+    
+    Cli.set_title("Autogit")
     
     args = parser.parse_args()
     action_mapper = {
