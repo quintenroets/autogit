4c7c072b29a1b15dfda10e8c187d2c82e6613172
Quinten <quinten.roets@gmail.com>
2022-11-09 20:08:39 -0500

    add push hook and disable custom pre-commit hook

--- autogit/pre_commit_custom.py
+++ autogit/pre_commit_custom.py
@@ -7,7 +7,7 @@ def main():
     commands = (command, f"{command} --hook-type pre-push")
     for command in commands:
         cli.run(command)
-    #project_root = cli.get("git rev-parse --show-toplevel")
-    #dest = Path(project_root) / ".git" / "hooks" / "pre-commit"
-    #template = Path.assets / "autogit" / "pre-commit-template"
-    #template.copy_to(dest)
+    # project_root = cli.get("git rev-parse --show-toplevel")
+    # dest = Path(project_root) / ".git" / "hooks" / "pre-commit"
+    # template = Path.assets / "autogit" / "pre-commit-template"
+    # template.copy_to(dest)
